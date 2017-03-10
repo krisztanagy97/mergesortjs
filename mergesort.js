2d77965ed0b1e4636f0c42a057fe96bf3098ca7c
krisztanagy97 <kriszta.nagy@emanuel.ro>
2017-03-10 08:27:24 +0200

    add the code

---
+++ mergesort.js
@@ -0,0 +1,29 @@
+ function mergeSort(a) {
+   if (a.length < 2) {
+     return a;
+   } else {
+     var mijloc = Math.trunc(a.length/2);
+     console.log(mijloc);
+     var stanga = a.slice(0, mijloc);
+     var dreapta = a.slice(mijloc, a.length);
+   }
+   return merge(mergeSort(stanga), mergeSort(dreapta));
+ }
+ function merge(s, d) {
+  var result = [];
+   while (s.length && d.length) {
+     if (s[0] <= d[0]) {
+       result.push(s.shift());
+     } else {
+       result.push(d.shift());
+     }
+   }
+   while (s.length) {
+     result.push(s.shift());
+   }
+   while (d.length) {
+     result.push(d.shift());
+   }
+   return result;
+ }
+console.log(mergeSort([2, 3, 1,5]));
